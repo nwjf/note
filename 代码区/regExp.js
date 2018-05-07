531c1fdb7e12f0da021dbad1dd4e90125ffb52f3
wjf <newwangjunfa@outlook.com>
2018-05-07 18:05:59 +0800

    'gs-2018-5-7'

--- 代码区/regExp.js
+++ 代码区/regExp.js
@@ -1,8 +1,8 @@
 /*
  * @Author: wjf 
  * @Date: 2018-01-14 14:50:23 
- * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-01-14 17:42:06
+ * @Last Modified by: wjf
+ * @Last Modified time: 2018-05-03 14:33:56
  */
 
 // 文本（最短长度1）
@@ -19,7 +19,10 @@ export const emailReg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9-])+((\.[a-zA-Z0-9-]{2,3})
 export const pwdReg = /^\d{6}$/;
 // 登录密码（6-20位数字字母_-@#*+.）
 export const loginPwdReg = /^[0-9a-zA-Z#@*-+_\.]{6,20}$/;
-
+// 身份证正则,大致匹配，
+export const idCardReg = /^[1-9]\d{9}(0[1-9]|1[12])(0[1-9]|[1-2][0-9]|3[01])\d{3}[0-9Xx]$/;
+// 大于0的数字，包括小树
+export const maxNumReg = /(^[1-9]\d*$)|(^[1-9]\d*\.\d*$)|(^0\.[1-9]\d*$)/;
 /*
  * 在下不才，如有不对，请小做修改
  * 如有不能满足需求的请添加
